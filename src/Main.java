82796a7eb77f28e97c123aebe5e14b00e03e6964
Kota Raghava <raghavendraasdf@gmail.com>
2025-05-07 08:20:59 +0530

    Trigger GitHub Actions run

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub Actions!");
+        System.out.println("Hello from GitHub Actions!"); // This is a test push to trigger GitHub Actions
+
     }
 }
